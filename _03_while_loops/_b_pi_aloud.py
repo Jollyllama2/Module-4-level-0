5e40f595ba244ff7f9aa75ecb5727b917071698f
ashwinsingh <ashwinjsingh012@gmail.com>
2024-01-07 16:04:33 -0800

    qwert

--- _03_while_loops/_b_pi_aloud.py
+++ _03_while_loops/_b_pi_aloud.py
@@ -10,12 +10,17 @@ if __name__ == '__main__':
     window.withdraw()
     # TODO) Place the first 20 digits of pi into a variable as a string
     #  3.1415926535897932384
+    Pi = '3.1415926535897932384'
 
     # TODO) Print out the first 3 digits of pi. For example,
     #  pi_str[0]   # first digit
     #  pi_str[1]   # second digit
-
+    print(Pi[0:4])
     # TODO) Use a while loop to keep asking for the next digit of pi
+    stop = 0
+    while len(Pi) > stop:
+        print(Pi[stop])
+        stop = stop+1
 
         # TODO) If they are correct, print "correct".
         #  If they are not, print "incorrect" and break out of the while loop
