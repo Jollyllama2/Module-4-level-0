05d9c0fa545a3a8e18fd1c81271e63ece4e98dcc
ashwinsingh <ashwinjsingh012@gmail.com>
2024-01-14 12:39:20 -0800

    qwert

--- _03_while_loops/_c_vending_machine.py
+++ _03_while_loops/_c_vending_machine.py
@@ -6,7 +6,7 @@ from tkinter import messagebox, simpledialog, Tk
 
 # Write you code under the if __name__ == '__main__': below
 def vending_machine(money):
-    items_for_sale = {"water" : 0.50, "soda" : 1.00, "pretzels" : 1.00, "candy bar" : 1.50, "exit" : 0.00}
+    items_for_sale = {"water" : 1.00, "soda" : 4.00, "pretzels" : 3.00, "candy bar" : 5.50, "exit" : 0.00}
 
     while True:
         intro_str = "Welcome to the vending machine! You have " +\
@@ -34,22 +34,32 @@ if __name__ == '__main__':
     window = Tk()
     window.withdraw()
 
-    money_in_dollars = 3.00
+    money_in_dollars = 7.50
 
     # TODO) Write a while loop that ends when you have no money left
+    while money_in_dollars >= 0:
+        money_spent = vending_machine(money_in_dollars)
+        money_spent = float(money_spent)
+        money_in_dollars -= money_spent
+        if money_in_dollars <= 0:
+            simpledialog.askstring(title='hi', prompt= ' U have no money')
+         #TODO) Call the vending_machine() function and save the money spent
+                 #  in a variable, for example:
+             #  money_spent = vending_machine(money_in_dollars)
+    money_spent = vending_machine(money_in_dollars)
 
-        # TODO) Call the vending_machine() function and save the money spent
-        #  in a variable, for example:
-        #  money_spent = vending_machine(money_in_dollars)
-
-        # TODO) If no money was spent, tell the user how much money they still
+         # TODO) If no money was spent, tell the user how much money they still
         #  have and exit the while loop
-
+    money_in_dollars = money_in_dollars
         # TODO) Otherwise, subtract the money spent from the amount of money
         #  you still have (money_in_dollars)
-
+    quotient =  money_in_dollars - money_spent
+    messagebox.showinfo(title='Amongi fungi',message= 'Thank u for ur purchase')
     # TODO) If there is exactly 0 money left, create a message that
     #  congratulates the user because they maximized their money.
-
+    if money_in_dollars == 0:
+        messagebox.showinfo(title='hi', message= '')
     # TODO) If there is a negative amount of money, tell the user they
     #  overspent!
+    if money_in_dollars < 0:
+        messagebox.showinfo(title='o',message='`U HAVE OVERSPENT, THINK BRO THINK')
